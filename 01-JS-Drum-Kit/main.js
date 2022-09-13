fd5cb8e2a213d01fe2d68412c5d55c1513ca8378
RamniwasMahala007 <71785474+RamniwasMahala007@users.noreply.github.com>
2022-09-13 12:34:52 +0530

    updating code in js file of sound light effect

--- 01-JS-Drum-Kit/main.js
+++ 01-JS-Drum-Kit/main.js
@@ -1,15 +1,33 @@
-window.addEventListener('keydown',(e)=>{                             //yaha par mene direct function call karaya hai
+window.addEventListener('keydown',function(e){                             //yaha par mene direct function call karaya hai
 
-    var sound=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const sound=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  // console.log(sound);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-    if(sound)
+    
+  //console.log(key);
+    if(!sound)
     {
-        sound.play();
-       sound.currentTime = 0;
+        return;
     }
+      
+       sound.currentTime = 0;
+       sound.play();
+       key.classList.add('playing');
+})
 
+const keys = document.querySelectorAll('.key');
+//console.log(keys);
+keys.forEach(function(key){
 
+    //console.log(e);
+    key.addEventListener('transitionend' , function(e){
+        //console.log(e); 
 
-
-
+        if(e.propertyName !== "transform"){
+            return;   
+        }
+        
+        this.classList.remove('playing');
+    })
 })
